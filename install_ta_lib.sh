fb5222b3df3d10dc3f84351f4730f54f6ec42395
Zagor <zagoreczek@gmail.com>
2024-05-26 13:46:08 +0200

    Update TA_Lib installation for Render

---
+++ install_ta_lib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Install dependencies
+apt-get update
+apt-get install -y build-essential
+apt-get install -y python3-dev
+apt-get install -y libta-lib0-dev
+apt-get install -y libta-lib0
+
+# Install TA_Lib
+pip install TA_Lib
